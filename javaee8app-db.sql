1bea22f2626604ed1f943fd2e102c4e42dffce31
Pablo Lorenzo Manzano <plorenzo.manzano@gmail.com>
2022-06-26 16:20:17 +0200

    Add Employee table

--- javaee8app-db.sql
+++ javaee8app-db.sql
@@ -1,3 +1,14 @@
 -- MySQL 8.0 database creation script.
 
 CREATE DATABASE javaee8app;
+
+USE javaee8app;
+
+CREATE TABLE Employee (
+    EmployeeId TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    Name VARCHAR(20) NOT NULL,
+    LastName VARCHAR(40) NOT NULL,
+    Position VARCHAR(40) NOT NULL,
+    GrossSalary MEDIUMINT UNSIGNED NOT NULL,
+    PRIMARY KEY (EmployeeId)
+);
